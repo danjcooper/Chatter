cc6928ca1565d98a6205ffb709f91f5836fb60f6
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-14 16:59:23 +0100

    init

---
+++ src/Components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#">I'm a link</a>
+      <a href="#">I'm a link</a>
+      <a href="#">I'm a link</a>
+      <a href="#">I'm a link</a>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components.
+import {Nav} from "./Components"
+
+const App = () => {
+    return (
+        <>
+        <Nav/>
+        <h1>I want to be famous</h1>
+        </>
+    )
+}
+
+export default App;
