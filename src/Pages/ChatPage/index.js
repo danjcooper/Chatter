db98ad2896ac039e5ed626cfcba88e83124671fb
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-02-26 16:15:42 +0000

    update server address

--- src/Pages/ChatPage/index.js
+++ src/Pages/ChatPage/index.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect} from "react";
-import { io } from "socket.io-client";
-import styles from "./style.module.css";
-import "./style.css"
-import { ChatBox } from "../../Components";
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import styles from './style.module.css';
+import './style.css';
+import { ChatBox } from '../../Components';
 
-const socket = io("https://chatter-api-dc.herokuapp.com/");
+const socket = io('https://chatter-app-socket.herokuapp.com/');
 
 const ChatPage = () => {
-  const [chatHistory, setChatHistory] = useState([{username: "Chatter", message: "hey, welcome to the room."}]);
-
-
+  const [chatHistory, setChatHistory] = useState([
+    { username: 'Chatter', message: 'hey, welcome to the room.' },
+  ]);
 
   useEffect(() => {
-    socket.on("msgfromsvr", (arg) => {
+    socket.on('msgfromsvr', (arg) => {
       addToChat(arg);
     });
   }, []);
 
-  
   const addToChat = (msg) => {
     setChatHistory((state) => {
       const newHistory = state.slice();
@@ -29,31 +28,37 @@ const ChatPage = () => {
   const sendChat = (e) => {
     e.preventDefault();
     const newMessage = e.target.newChat.value;
-    const username = localStorage.getItem("username") ? localStorage.getItem("username") : "stranger"
-  
-    
+    const username = localStorage.getItem('username')
+      ? localStorage.getItem('username')
+      : 'stranger';
+
     const op = {
       username: username,
-      message: newMessage
-    }
+      message: newMessage,
+    };
 
     addToChat(op);
-    socket.emit("send to server", op);
+    socket.emit('send to server', op);
 
-    e.target.newChat.value = "";
+    e.target.newChat.value = '';
   };
 
   return (
-    <div id="chatContainer">
+    <div id='chatContainer'>
       <div className={styles.chatMessages}>
         <ChatBox chatArray={chatHistory} />
       </div>
 
       <div className={styles.chatInput}>
         <form onSubmit={(e) => sendChat(e)}>
-          <label for="newChat"></label>
-          <input className={styles.newMessage} id="newChat" type="text" placeholder="type chat here" />
-          <input className={styles.sendButton} type="submit" value="Send" />
+          <label for='newChat'></label>
+          <input
+            className={styles.newMessage}
+            id='newChat'
+            type='text'
+            placeholder='type chat here'
+          />
+          <input className={styles.sendButton} type='submit' value='Send' />
         </form>
       </div>
     </div>
